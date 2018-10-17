41e6580b2ee973717c165f0bfe3732f6b74dd0e2
Ankush Sharma <ankush.sharma_cs16@gla.ac.in>
2018-10-17 22:23:53 +0530

    Create leap year check.py

---
+++ leap year check.py
@@ -0,0 +1,16 @@
+def checkYear(year): 
+    if (year % 4) == 0: 
+        if (year % 100) == 0: 
+            if (year % 400) == 0: 
+                return True
+            else: 
+                return False
+        else: 
+             return True
+    else: 
+        return False
+year = int(input())
+if(checkYear(year)): 
+    print("yes") 
+else: 
+    print("no") 
